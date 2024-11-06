559f67f5cddce143cd271b4c1d465f12ded5e128
maryammaz <144738938+maryammaz@users.noreply.github.com>
2024-11-06 04:59:42 +0000

    added a game_ideas file and brainstormed on what mini games our website will include

---
+++ game_ideas.py
@@ -0,0 +1,16 @@
+# Brainstorming Game Ideas:
+
+### 1. Tic-Tac-Toe game
+# Theme: It could be customized with touches relating to the series, like using cute doodles or character icons (ie. hearts in place of X).
+# For Variation, we could have single player mode where players could compete against AI. The other option would be multiplayer mode.
+# Design: We could style the backdrop with bright, on theme colors and doodle-like borders to capture the essence of Dork Diaries.
+
+### 2. Mini-Puzzle Game 
+# Simple puzzles with images related to dork diaries, which can be pieced together in either hard or easy mode.
+
+### 3. Doodle Art Game
+# A digital space to draw with different color options and brush sizes. Players could create designs and save them or clear to start over.
+
+### 4. Match the Character Game 
+# A memory card game with pairs of Dork Diaries characters and their accesories (?). 
+# Goal: find matches to complete the game.
